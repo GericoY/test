e67f233e2ddfff371581ceb9dcd3a14f341eeca7
GericoY <gericoyutuc@gmail.com>
2022-11-16 18:01:49 +0800

    Added Array

--- shopping_cart/index.php
+++ shopping_cart/index.php
@@ -1,3 +1,64 @@
+<?php
+    $arrProducts = [
+        [
+            'name' => "Brown Shirt",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "550";
+            'photo1' => "product1A.jpg",
+            'photo2' => "product1B.jpg"
+        ],
+        [
+            'name' => "Gray Shirt",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "550";
+            'photo1' => "product2A.jpg",
+            'photo2' => "product2B.jpg"
+        ],
+        [
+            'name' => "White Blazer",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "750";
+            'photo1' => "product3A.jpg",
+            'photo2' => "product3B.jpg"
+        ],
+        [
+            'name' => "Dark Blue Polo Shirt",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "600";
+            'photo1' => "product4A.jpg",
+            'photo2' => "product4B.jpg"
+        ],
+        [
+            'name' => "Dark Blue Long Sleeves",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "800";
+            'photo1' => "product5A.jpg",
+            'photo2' => "product5B.jpg"
+        ],
+        [
+            'name' => "White Long Sleeves",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "800";
+            'photo1' => "product6A.jpg",
+            'photo2' => "product6B.jpg" 
+        ],
+        [
+            'name' => "Dark Blue Blazer",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "750";
+            'photo1' => "product7A.jpg",
+            'photo2' => "product7B.jpg"
+        ],
+        [
+            'name' => "Floral Polo",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "650";
+            'photo1' => "product8A.jpg",
+            'photo2' => "product8B.jpg"
+        ],
+    ]
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,19 +67,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../css/shopping-cart.css">
     <title>Home</title>
 </head>
 <body>
-    <div class="container">
-        <div class="col-8">
+    <div class="container mt-3">
+        <div class="col">
             <div class="row">
-                <h1><i class="fa-solid fa-store"></i> Learn IT Easy Online Shop</h1>
-                <a class="btn btn-primary" href="cart.php" role="button"><i class="fa-solid fa-cart-shopping"></i> Cart <span class="badge badge-light">4</span></a>
+                <h2><i class="fa-solid fa-store"></i> Learn IT Easy Online Shop </h2>
+                <div class="d-block">
+                    <a class="btn btn-primary" href="#" role="button"><i class="fa-solid fa-cart-shopping"></i> Cart <span class="badge badge-light">0</span></a>
+                </div>
             </div>
         </div>
+        <hr>
     </div>
     
-
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js" integrity="sha512-wV7Yj1alIZDqZFCUQJy85VN+qvEIly93fIQAN7iqDFCPEucLCeNFz4r35FCo9s6WrpdDQPi80xbljXB8Bjtvcg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js" integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
