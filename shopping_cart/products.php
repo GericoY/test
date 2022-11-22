9c3c285dccf758fe4669881f7ae155181e77a6e3
GericoY <gericoyutuc@gmail.com>
2022-11-22 10:33:03 +0800

    Done details.php

---
+++ shopping_cart/products.php
@@ -0,0 +1,63 @@
+<?php
+    session_start();
+
+    
+    $arrProducts = [
+        [
+            'name' => "Brown Shirt",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "550",
+            'photo1' => "img/produc1A.jpg",
+            'photo2' => "img/produc1B.jpg"
+        ],
+        [
+            'name' => "Gray Shirt",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "550",
+            'photo1' => "img/produc2A.jpg",
+            'photo2' => "img/produc2B.jpg"
+        ],
+        [
+            'name' => "White Blazer",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "750",
+            'photo1' => "img/produc3A.jpg",
+            'photo2' => "img/produc3B.jpg"
+        ],
+        [
+            'name' => "Dark Blue Polo Shirt",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "600",
+            'photo1' => "img/produc4A.jpg",
+            'photo2' => "img/produc4B.jpg"
+        ],
+        [
+            'name' => "Dark Blue Long Sleeves",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "800",
+            'photo1' => "img/produc5A.jpg",
+            'photo2' => "img/produc5B.jpg"
+        ],
+        [
+            'name' => "White Long Sleeves",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "800",
+            'photo1' => "img/produc6A.jpg",
+            'photo2' => "img/produc6B.jpg" 
+        ],
+        [
+            'name' => "Dark Blue Blazer",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "750",
+            'photo1' => "img/produc7A.jpg",
+            'photo2' => "img/produc7B.jpg"
+        ],
+        [
+            'name' => "Floral Polo",
+            'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque architecto et distinctio quasi reprehenderit, culpa nemo ipsum, similique asperiores officia cum. Numquam ab explicabo tempora quam assumenda. Aliquam, adipisci incidunt.",
+            'price' => "650",
+            'photo1' => "img/produc8A.jpg",
+            'photo2' => "img/produc8B.jpg"
+        ],
+    ]
+?>
